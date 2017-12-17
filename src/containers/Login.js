7b9f7726b737a5aadabe5f733db29837489c8ea2
Pablo Cordova <pccm_77@hotmail.com>
2017-12-17 01:26:00 -0500

    REFACTOR login style

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { login } from '../actions/login'
-import { FormControl, ControlLabel, FormGroup } from 'react-bootstrap'
+import { FormControl, ControlLabel, FormGroup, Row, Grid, Col } from 'react-bootstrap'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import RaisedButton from 'material-ui/RaisedButton'
 
@@ -18,42 +18,48 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <h2>INICIAR SESIÓN</h2>
-        <MuiThemeProvider>
-          <form>
-            <FormGroup>
-              <ControlLabel>Correo:</ControlLabel>
-              <FormControl
-                type = 'text'
-                placeholder = 'Ingrese Correo'
-                onChange = { (event) => {
-                  this.setState({
-                    email: event.target.value
-                  })
-                }}
-              />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Contraseña</ControlLabel>
-              <FormControl
-                type = 'password'
-                placeholder = 'Ingrese Contraseña'
-                onChange = { (event) => {
-                  this.setState({
-                    pass: event.target.value
-                  })
-                }}
-              />
-            </FormGroup>
-            <FormGroup>
-              <RaisedButton
-                label = 'Entrar'
-                primary = { true }
-                onClick = { () => { this.props.login(this.state.email, this.state.pass) } }
-              />
-            </FormGroup>
-          </form>
-        </MuiThemeProvider>
+        <Grid>
+          <Row>
+            <Col xs = { 4 } xsOffset = { 4 }>
+              <h2>INICIAR SESIÓN</h2>
+              <MuiThemeProvider>
+                <form>
+                  <FormGroup>
+                    <ControlLabel>Correo:</ControlLabel>
+                    <FormControl
+                      type = 'text'
+                      placeholder = 'Ingrese Correo'
+                      onChange = { (event) => {
+                        this.setState({
+                          email: event.target.value
+                        })
+                      }}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <ControlLabel>Contraseña</ControlLabel>
+                    <FormControl
+                      type = 'password'
+                      placeholder = 'Ingrese Contraseña'
+                      onChange = { (event) => {
+                        this.setState({
+                          pass: event.target.value
+                        })
+                      }}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <RaisedButton
+                      label = 'Entrar'
+                      primary = { true }
+                      onClick = { () => { this.props.login(this.state.email, this.state.pass) } }
+                    />
+                  </FormGroup>
+                </form>
+              </MuiThemeProvider>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
